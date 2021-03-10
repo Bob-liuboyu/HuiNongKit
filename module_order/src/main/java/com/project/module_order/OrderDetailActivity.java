f1e37130b9eb88c1950c6e754512b529a6e945db
liuboyu <liu.boyu_3334@immomo.com>
2021-03-10 18:32:10 +0800

    权限差不多了

--- module_order/src/main/java/com/project/module_order/OrderDetailActivity.java
+++ module_order/src/main/java/com/project/module_order/OrderDetailActivity.java
@@ -1,27 +1,26 @@
 package com.project.module_order;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.project.arch_repo.base.activity.BaseActivity;
-import com.project.arch_repo.utils.SystemUtils;
 import com.project.config_repo.ArouterConfig;
 import com.project.module_order.databinding.OrderActivityDetailBinding;
-import com.xxf.arch.XXF;
-import com.xxf.arch.rxjava.transformer.CameraPermissionTransformer;
 
-import java.util.Arrays;
-
-import io.reactivex.functions.Consumer;
-//import permissions.dispatcher.NeedsPermission;
-//import permissions.dispatcher.OnNeverAskAgain;
-//import permissions.dispatcher.OnPermissionDenied;
-//import permissions.dispatcher.OnShowRationale;
-//import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
 /**
  * @fileName: OrderDetailActivity
@@ -29,51 +28,87 @@ import io.reactivex.functions.Consumer;
  * @date: 2021/3/10 2:50 PM
  * @description: 订单详情
  */
-public class OrderDetailActivity extends BaseActivity {
-//    private OrderActivityDetailBinding binding;
-//    private static final int REQUEST_CODE_SETTING = 1;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        binding = OrderActivityDetailBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//        setListener();
-//    }
-//
-//    public void setListener() {
-//        if (binding == null) {
-//            return;
-//        }
-//        binding.btnCamera.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                showCamera();
-//            }
-//        });
-//    }
-//
-//    @NeedsPermission(Manifest.permission.CAMERA)
-//    public void showCamera() {
-//    }
-//
-//    @OnShowRationale(Manifest.permission.CAMERA)
-//    public void showRationale(final PermissionRequest permissionRequest) {
-//        permissionRequest.proceed();
-//    }
-//
-//    @OnNeverAskAgain(Manifest.permission.CAMERA)
-//    public void neverAsk() {
-//        Toast.makeText(this, "您拒绝了相机权限，且不再询问，请前往设置中心授权", Toast.LENGTH_LONG).show();
-//    }
-//
-//    @OnPermissionDenied(Manifest.permission.CAMERA)
-//    public void denied() {
-//        Toast.makeText(this, "您拒绝了相机权限，该功能不可用", Toast.LENGTH_LONG).show();
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//    }
+@Route(path = ArouterConfig.Order.ORDER_DETAIL)
+@RuntimePermissions
+public class OrderDetailActivity extends AppCompatActivity {
+    private OrderActivityDetailBinding binding;
+    private static final int REQUEST_CODE_SETTING = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = OrderActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListener();
+    }
+
+    public void setListener() {
+        if (binding == null) {
+            return;
+        }
+        binding.btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderDetailActivityPermissionsDispatcher.showCameraWithCheck(OrderDetailActivity.this);
+            }
+        });
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    public void showCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// 启动系统相机
+        startActivityForResult(intent, 12);
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(PermissionRequest request) {
+        // NOTE: Show a rationale to explain why the permission is needed, e.g. with a dialog.
+        // Call proceed() or cancel() on the provided PermissionRequest to continue or abort
+        showRationaleDialog("permission_camera_rationale", request);
+    }
+
+    @OnShowRationale({Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS})
+    void showRationaleForContact(PermissionRequest request) {
+        // NOTE: Show a rationale to explain why the permission is needed, e.g. with a dialog.
+        // Call proceed() or cancel() on the provided PermissionRequest to continue or abort
+        showRationaleDialog("permission_contacts_rationale", request);
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void onCameraDenied() {
+        // NOTE: Deal with a denied permission, e.g. by showing specific UI
+        // or disabling certain functionality
+        Toast.makeText(this, "permission_camera_denied", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void onCameraNeverAskAgain() {
+        Toast.makeText(this, "permission_camera_never_askagain", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showRationaleDialog(String message, final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton("允许", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("拒绝", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage(message)
+                .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        OrderDetailActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
 }
